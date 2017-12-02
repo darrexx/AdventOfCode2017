439bc0ab0dfc179ab985713737e7849117197fbf
darrexx <timo.walter@udo.edu>
2017-12-02 12:28:22 +0100

    Add Solution for Day02

---
+++ Day02/Day02.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCodeInputReader;
+using Day02.Properties;
+
+namespace Day02
+{
+    class Day02
+    {
+        static void Main(string[] args)
+        {
+            var aocInputReader = new AoCInputReader(Settings.Default.SessionCookie);
+            var lines = aocInputReader.GetInputForDay(2).Result;
+
+            var checksum = 0;
+            foreach (var line in lines)
+            {
+                var values = line.Split('\t').Where(x => x != String.Empty).Select(Int32.Parse).ToArray();
+                checksum += values.Max() - values.Min();
+            }
+            Console.WriteLine(checksum);
+        }
+    }
+}
---
+++ Day02_2/Day02_2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCodeInputReader;
+using Day02_2.Properties;
+
+namespace Day02_2
+{
+    class Day02_2
+    {
+        static void Main(string[] args)
+        {
+            var aocInputReader = new AoCInputReader(Settings.Default.SessionCookie);
+            var lines = aocInputReader.GetInputForDay(2).Result;
+
+            var checksum = 0;
+            foreach (var line in lines)
+            {
+                var values = line.Split('\t').Where(x => x != String.Empty).Select(Int32.Parse).ToArray();
+                for (var i = 0; i < values.Length; i++)
+                {
+                    for (var j = i+1 ; j < values.Length; j++)
+                    {
+                        if (values[i] % values[j] == 0)
+                        {
+                            checksum += values[i] / values[j];
+                        }
+                        if (values[j] % values[i] == 0)
+                        {
+                            checksum += values[j] / values[i];
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(checksum);
+        }
+    }
+}
