1dae74560c586430d28efdc1d32a0cd42b49cac9
darrexx <timo.walter@udo.edu>
2017-12-03 13:31:46 +0100

    Add Day03 Part 1

---
+++ Day03/Day03.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day03
+{
+    class Day03
+    {
+        static void Main(string[] args)
+        {
+            var input = 289326;
+            var counter = 0;
+            var ebene = 1;
+
+            while (counter <= input)
+            {
+                counter = (int) Math.Pow(ebene, 2);
+                ebene = ebene + 2;
+            }
+            ebene = ebene / 2;
+            var top = 4;
+            var bottom = 8;
+            var left = 6;
+            var right = 2;
+            for (int i = 1; i <= ebene-2; i++)
+            {
+                top += 3 + i * 8;
+                bottom += 7 + i * 8;
+                left += 5 + i * 8;
+                right += 1 + i * 8;
+            }
+            var arr = new[] {top, bottom, left, right};
+            var min = arr.Min(x => Math.Abs(x - input));
+            Console.WriteLine(ebene-1 +min);
+        }
+    }
+}
