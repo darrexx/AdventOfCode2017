027fe044368bd0d8b465b6d639f3f8e49d2c8faf
darrexx <timo.walter@udo.edu>
2017-12-05 10:40:40 +0100

    Add Solution for Day05.

---
+++ Day05/Day05.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using AdventOfCodeInputReader;
+using Day05.Properties;
+
+namespace Day05
+{
+    class Day05
+    {
+        static void Main(string[] args)
+        {
+            var aocInputReader = new AoCInputReader(Settings.Default.SessionCookie);
+            var lines = aocInputReader.GetInputForDay(5).Result;
+
+            var digits = lines.Select(Int32.Parse).ToArray();
+            var position = 0;
+            var isEnded = false;
+            var steps = 0;
+            while (!isEnded)
+            {
+                digits[position] = digits[position] + 1;
+                position = position + digits[position] - 1;
+                steps++;
+                if (position >= digits.Length || position < 0)
+                {
+                    isEnded = true;
+                }
+            }
+            Console.WriteLine(steps);
+        }
+    }
+}
---
+++ Day05_2/Day05_2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using AdventOfCodeInputReader;
+using Day05_2.Properties;
+
+namespace Day05_2
+{
+    class Day05_2
+    {
+        static void Main(string[] args)
+        {
+            var aocInputReader = new AoCInputReader(Settings.Default.SessionCookie);
+            var lines = aocInputReader.GetInputForDay(5).Result;
+
+            var digits = lines.Select(Int32.Parse).ToArray();
+            var position = 0;
+            var isEnded = false;
+            var steps = 0;
+            while (!isEnded)
+            {
+                if (digits[position] >= 3)
+                {
+                    digits[position] = digits[position] - 1;
+                    position = position + digits[position] + 1;
+                }
+                else
+                {
+                    digits[position] = digits[position] + 1;
+                    position = position + digits[position] - 1;
+                }
+                
+                steps++;
+                if (position >= digits.Length || position < 0)
+                {
+                    isEnded = true;
+                }
+            }
+            Console.WriteLine(steps);
+        }
+    }
+}
