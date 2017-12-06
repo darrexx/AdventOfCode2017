fdac3cbedac5c9837011be6014f27a5130c4016d
darrexx <timo.walter@udo.edu>
2017-12-06 13:23:04 +0100

    Add Solution for Day06

---
+++ Day06/Day06.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCodeInputReader;
+using Day06.Properties;
+
+namespace Day06
+{
+    class Day06
+    {
+        static void Main(string[] args)
+        {
+            var aocInputReader = new AoCInputReader(Settings.Default.SessionCookie);
+            var lines = aocInputReader.GetInputForDay(6).Result;
+
+            var memorybanks = lines.SelectMany(x => x.Split('\t')).Where(x => x != String.Empty).Select(Int32.Parse).ToArray();
+            var rememberedStates = new List<int[]>();
+            var steps = 0;
+
+            do
+            {
+                rememberedStates.Add((int[])memorybanks.Clone());
+                var toDistribute = memorybanks.Max();
+                var currentIndex = Array.IndexOf(memorybanks, toDistribute);
+                memorybanks[currentIndex] = 0;
+
+                for (var i = 0; i < toDistribute; i++)
+                {
+                    currentIndex++;
+                    if (currentIndex >= memorybanks.Length)
+                    {
+                        currentIndex = 0;
+                    }
+
+                    memorybanks[currentIndex] += 1;
+                }
+                steps++;
+
+            } while (rememberedStates.All(x => !x.SequenceEqual(memorybanks)));
+
+            Console.WriteLine(steps);
+        }
+    }
+}
---
+++ Day06_2/Day06_2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCodeInputReader;
+using Day06_2.Properties;
+
+namespace Day06_2
+{
+    class Day06_2
+    {
+        static void Main(string[] args)
+        {
+            var aocInputReader = new AoCInputReader(Settings.Default.SessionCookie);
+            var lines = aocInputReader.GetInputForDay(6).Result;
+
+            var memorybanks = lines.SelectMany(x => x.Split('\t')).Where(x => x != String.Empty).Select(Int32.Parse).ToArray();
+            var rememberedStates = new List<int[]>();
+            var steps = 0;
+            var hasFoundDuplicateState = false;
+
+            do
+            {
+                rememberedStates.Add((int[])memorybanks.Clone());
+                var toDistribute = memorybanks.Max();
+                var currentIndex = Array.IndexOf(memorybanks, toDistribute);
+                memorybanks[currentIndex] = 0;
+
+                for (var i = 0; i < toDistribute; i++)
+                {
+                    currentIndex++;
+                    if (currentIndex >= memorybanks.Length)
+                    {
+                        currentIndex = 0;
+                    }
+
+                    memorybanks[currentIndex] += 1;
+                }
+                steps++;
+
+                if (rememberedStates.Any(x => x.SequenceEqual(memorybanks)) && !hasFoundDuplicateState)
+                {
+                    rememberedStates.Clear();
+                    hasFoundDuplicateState = true;
+                    steps = 0;
+                }
+
+            } while (rememberedStates.All(x => !x.SequenceEqual(memorybanks)));
+
+            Console.WriteLine(steps);
+        }
+    }
+}
