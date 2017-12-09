dd46b8aeb7236e5a4e66aac0bb7dbb1f75fef491
darrexx <timo.walter@udo.edu>
2017-12-09 16:26:01 +0100

    Add Solution for Day09

---
+++ Day09/Day09.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using AdventOfCodeInputReader;
+using Day09.Properties;
+
+namespace Day09
+{
+    class Day09
+    {
+        static void Main(string[] args)
+        {
+            var aocInputReader = new AoCInputReader(Settings.Default.SessionCookie);
+            var lines = aocInputReader.GetInputForDay(9).Result;
+
+            var level = 0;
+            var result = 0;
+            var isInGarbageBrackets = false;
+
+            var line = lines[0];
+            for (var i = 0; i < line.Length; i++)
+            {
+                switch (line[i])
+                {
+                    case '<':
+                        isInGarbageBrackets = true;
+                        break;
+                    case '>':
+                        isInGarbageBrackets = false;
+                        break;
+                    case '{' when !isInGarbageBrackets:
+                        level++;
+                        break;
+                    case '}' when !isInGarbageBrackets:
+                        result += level;
+                        level--;
+                        break;
+                    case '!':
+                        i++;
+                        break;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Day09_2/Day09_2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using AdventOfCodeInputReader;
+using Day09_2.Properties;
+
+namespace Day09_2
+{
+    class Day09_2
+    {
+        static void Main(string[] args)
+        {
+            var aocInputReader = new AoCInputReader(Settings.Default.SessionCookie);
+            var lines = aocInputReader.GetInputForDay(9).Result;
+
+            var result = 0;
+            var isInGarbageBrackets = false;
+
+            var line = lines[0];
+            for (var i = 0; i < line.Length; i++)
+            {
+                switch (line[i])
+                {
+                    case '<':
+                        if (isInGarbageBrackets)
+                        {
+                            result++;
+                        }
+                        isInGarbageBrackets = true;
+                        break;
+                    case '>':
+                        isInGarbageBrackets = false;
+                        break;
+                    case '!':
+                        i++;
+                        break;
+                    default:
+                        if (isInGarbageBrackets)
+                        {
+                            result++;
+                        }
+                        break;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
